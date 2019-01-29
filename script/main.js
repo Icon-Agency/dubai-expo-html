0018710880ab6bb51532515b86acce39adaccad2
Nina Li <nina.li@iconagency.com.au>
2019-01-30 10:09:27 +1100

    First commit

---
+++ script/main.js
@@ -0,0 +1,47 @@
+$(function() {
+	$.scrollify({
+		section: ".panel",
+		sectionName: false,
+		interstitialSection: ".outofscroll, .extra, .footer",
+		easing: "easeOutExpo",
+		scrollSpeed: 1100,
+		offset: 0,
+		scrollbars: true,
+		standardScrollElements: "",
+		setHeights: false,
+		overflowScroll: true,
+		updateHash: true,
+		touchScroll: true,
+		before: function(i, panels) {
+			var active = $(".slide.active");
+			var ref = panels[i].attr("data-section-name");
+			$(".pagination a").css("display", "block");
+
+			$(".pagination .active").removeClass("active");
+			$(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+			active.addClass("remove");
+
+			//setTimeout(function() {
+			$("[data-slide=" + i + "]").addClass("active");
+			active.removeClass("remove active");
+			//},300);
+
+			// $('".' + ref+'"').addClass("actived");
+
+
+		},
+		after: function() {
+			
+		},
+		afterResize: function() {
+			
+		},
+		afterRender: function() {
+			$(".pagination a").css("display", "none");
+			$(".pagination a").on("click",$.scrollify.move);
+
+		}
+
+	});
+
+});
